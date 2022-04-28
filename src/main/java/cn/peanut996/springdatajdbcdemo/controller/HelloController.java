02eecdb164d9be62e88ec94cfe20b5fd1c912be8
Peanuts <849421294godw@gmail.com>
2022-04-28 00:58:27 +0000

    create hello controller

---
+++ src/main/java/cn/peanut996/springdatajdbcdemo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cn.peanut996.springdatajdbcdemo.controller;
+
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello World!";
+  }
+  
+}
